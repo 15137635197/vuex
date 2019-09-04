31e8d73cd755115f854873dc027946062fc0dc5d
chenxiuxiu <wb_chenxiuxiu@kuaishou.com>
2019-09-04 17:00:33 +0800

    测试

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -4,6 +4,7 @@
       <div class="box">
         <h1>{{city}}</h1>
         <h4>gitlens的用法</h4>
+        <p>合远程做比较</p>
         <router-link tag="button" to="/citylist">获取城市列表</router-link>
       </div>
   </div>
